503d466ef477a8f5a7b38fb679b3ad5e65ac04ad
Dario Berzano <dario.berzano@cern.ch>
2019-09-10 10:05:27 +0200

    Check for spurious .md files at PR level already
    This is a better fix than #77. Spurious .md files must not exist. Only .md files
    in `SUMMARY.md` must be present and rendered. All files not intended to be
    rendered must not be in the repository.

--- publish.sh
+++ publish.sh
@@ -12,7 +12,6 @@ pushd _book
   git init .
   git config user.name 'ALICE Doc Bot'
   git config user.email 'alice-doc-bot@cern.ch'
-  find . -name '*.md' -delete || true
   git add --all
   git commit -m 'Update site'
   git push -f https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG HEAD:gh-pages
