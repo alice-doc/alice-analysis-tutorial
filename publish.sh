ede9d0e1bf02a1569eff5224a2d7b24cc1adb813
mpuccio <mpuccio@users.noreply.github.com>
2020-08-26 15:53:41 +0000

    Deploying to gh-pages from  @ 62ce262a5123db06bbb78d0244b1ddbd35e23a87 🚀

--- publish.sh
+++ publish.sh
@@ -4,15 +4,14 @@ cd "$(dirname "$0")"
 
 [[ -r env.sh ]] && source env.sh &> /dev/null
 
-[[ $TRAVIS_REPO_SLUG ]]             || exit 1
-[[ $TRAVIS_PULL_REQUEST == false ]] || exit 1
-[[ $GH_TOKEN ]]                     || exit 1
+PUSHURL=$(git remote get-url --push origin)
 
 pushd _book
+  touch .nojekyll
   git init .
   git config user.name 'ALICE Doc Bot'
   git config user.email 'alice-doc-bot@cern.ch'
   git add --all
   git commit -m 'Update site'
-  git push -f https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG HEAD:gh-pages
+  git push -f $PUSHURL HEAD:gh-pages
 popd
