012b6894aac440f4215a3a666d74c0c9ccd1b6ae
Maximiliano Puccio <mpuccio@users.noreply.github.com>
2020-08-26 16:59:32 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -4,9 +4,7 @@ cd "$(dirname "$0")"
 
 [[ -r env.sh ]] && source env.sh &> /dev/null
 
-[[ $TRAVIS_REPO_SLUG ]]             || exit 1
-[[ $TRAVIS_PULL_REQUEST == false ]] || exit 1
-[[ $GH_TOKEN ]]                     || exit 1
+PUSHURL=$(git remote get-url --push origin)
 
 pushd _book
   git init .
@@ -14,5 +12,5 @@ pushd _book
   git config user.email 'alice-doc-bot@cern.ch'
   git add --all
   git commit -m 'Update site'
-  git push -f https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG HEAD:gh-pages
+  git push -f $PUSHURL HEAD:gh-pages
 popd
